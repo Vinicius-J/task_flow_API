694b1bd6b17a7f82e8aace552a1c458e0211114a
Vinícius Joaquim <viniciusjoaquimdossantos@gmail.com>
2026-07-20 23:26:17 -0300

    feat: add interface for repository in memory

--- src/data/InMemoryTaskRepository.js
+++ src/data/InMemoryTaskRepository.js
@@ -1,9 +1,21 @@
-const TaskRepository = [
+const tasks = [
   { id: 1, status: "inProgress", task: "Tarefa 1" },
   { id: 2, status: "completed", task: "Tarefa 2" },
   { id: 3, status: "inProgress", task: "Tarefa 3" },
   { id: 4, status: "completed", task: "Tarefa 4" },
   { id: 5, status: "completed", task: "Tarefa 5" },
 ];
+
+const TaskRepostitory = {
+  create(task) {
+    tasks.push(task);
+  },
+  findAll() {
+    return tasks;
+  },
+  findById(id) {
+    return tasks.find((task) => task.id === id);
+  },
+};
 
 export default TaskRepository;
