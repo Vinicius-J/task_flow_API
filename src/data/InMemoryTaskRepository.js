008ef4fbba3492f44e28601ad3b800f2fd36bee7
Vinícius Joaquim <viniciusjoaquimdossantos@gmail.com>
2026-07-21 01:03:15 -0300

    feat: add update in repository

--- src/data/InMemoryTaskRepository.js
+++ src/data/InMemoryTaskRepository.js
@@ -1,9 +1,10 @@
+import { TaskStatus } from "../utils/TaskStatus.js";
 let tasks = [
-  { id: 1, status: "inProgress", task: "Tarefa 1" },
-  { id: 2, status: "completed", task: "Tarefa 2" },
-  { id: 3, status: "inProgress", task: "Tarefa 3" },
-  { id: 4, status: "completed", task: "Tarefa 4" },
-  { id: 5, status: "completed", task: "Tarefa 5" },
+  { id: 1, status: TaskStatus.IN_PROGRESS, task: "Tarefa 1" },
+  { id: 2, status: TaskStatus.COMPLETED, task: "Tarefa 2" },
+  { id: 3, status: TaskStatus.IN_PROGRESS, task: "Tarefa 3" },
+  { id: 4, status: TaskStatus.COMPLETED, task: "Tarefa 4" },
+  { id: 5, status: TaskStatus.COMPLETED, task: "Tarefa 5" },
 ];
 
 const InMemoryTaskRepository = {
@@ -15,6 +16,11 @@ const InMemoryTaskRepository = {
   },
   findById(id) {
     return tasks.find((task) => task.id === Number(id));
+  },
+  update(id, updatedTask) {
+    tasks.forEach((task) => {
+      if (task.id === Number(id)) task.task = updatedTask;
+    });
   },
   delete(id) {
     const newTasks = tasks.filter((task) => task.id !== Number(id));
